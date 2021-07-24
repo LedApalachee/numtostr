5fa72c0c92d1c94f91f33aec5eff20fd4ed56765
LedApalachee <ledapalachee@gmail.com>
2021-07-25 00:44:00 +0700

    yes

--- numtostr.c
+++ numtostr.c
@@ -10,12 +10,22 @@ char* numtostr(int n)
 	for (int i = 10; n / i; i *= 10)
 		++digits_count;
 
-	string = (char*) malloc(sizeof(char) * digits_count + sizeof(char));
+	string = (char*) malloc(sizeof(char) * digits_count +  sizeof(char) * (n < 0) + sizeof(char));
 
-	for (int i = digits_count-1; i >= 0; --i)
-		string[digits_count - i - 1] = '0' + ((n / (int)pow(10, i)) % 10);
-
-	string[digits_count] = '\0';
+	if (n >= 0)
+	{
+		for (int i = digits_count-1; i >= 0; --i)
+			string[digits_count - i - 1] = '0' + ((n / (int)pow(10, i)) % 10);
+		string[digits_count] = '\0';
+	}
+	else
+	{
+		n *= -1;
+		for (int i = digits_count-1; i >= 0; --i)
+			string[digits_count - i] = '0' + ((n / (int)pow(10, i)) % 10);
+		string[0] = '-';
+		string[digits_count + 1] = '\0';
+	}
 
 	return string;
 }
